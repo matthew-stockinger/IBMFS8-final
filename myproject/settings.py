901b5677e768d7e770c9a749c008db32058eb043
Matthew Stockinger <matt@matthewstockinger.com>
2023-07-26 20:37:31 -0700

    add key environment var

--- myproject/settings.py
+++ myproject/settings.py
@@ -19,10 +19,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-# SECRET_KEY = "dbg4TBiYQuYZm_zrmC@mLF2y!ah.rH7jrRX6*z62i2xUpk637A"
-with open("/django-keys/ibmfs8-final-key.txt") as f:
-    SECRET_KEY = f.read().strip()
+# local dev:
+# with open("/django-keys/ibmfs8-final-key.txt") as f:
+#     SECRET_KEY = f.read().strip()
+SECRET_KEY = os.environ["SECRET_KEY"]
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
